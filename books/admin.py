6e22df1f66d9474c5649be6736065e6f4434e222
Chetan Ganji <monarch@dragonsden.black>
2024-12-06 10:57:49 +0530

    staging: Adds book related models

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
 
-# Register your models here.
+from books.models import Book, Review
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    search_fields = ["id"]
+
+    list_display_links = ["title"]
+
+    list_filter = ["is_recommended"]
+
+    list_display = [
+        "id",
+        "title",
+        "is_recommended",
+        "user",
+        "created_at",
+        "modified_at",
+    ]
+
+    list_per_page = 25
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    search_fields = ["id"]
+
+    list_display_links = ["book"]
+
+    list_filter = []
+
+    list_display = [
+        "id",
+        "book",
+        "user",
+        "rating",
+        "created_at",
+        "modified_at",
+    ]
+
+    list_per_page = 25
--- books/models.py
+++ books/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+from core.models import TimeStampedModel
+
+
+class Book(TimeStampedModel):
+    user = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE, related_name="contacts"
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    is_recommended = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.title}"
+
+    class Meta:
+        verbose_name = "Book"
+        verbose_name_plural = "Books"
+
+
+class Review(TimeStampedModel):
+    book = models.ForeignKey(
+        "books.Book", on_delete=models.CASCADE, related_name="reviews"
+    )
+    user = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE, related_name="reviews"
+    )
+    rating = models.IntegerField()
+    review = models.TextField()
+
+    def __str__(self):
+        return f"{self.id}"
+
+    class Meta:
+        verbose_name = "Review"
+        verbose_name_plural = "Reviews"
+        unique_together = ["book", "user"]
